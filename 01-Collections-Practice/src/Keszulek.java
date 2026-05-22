c992394c957708c58db1cf7ef926f7de4471b9d6
uakos03 <urbanakos12@gmail.com>
2026-05-22 15:23:02 +0200

    Konstruktor, Getter hozzaadva

--- 01-Collections-Practice/src/Keszulek.java
+++ 01-Collections-Practice/src/Keszulek.java
@@ -1,5 +1,17 @@
 public abstract class Keszulek {
         private String gyarto;
         private int ar;
-        
+
+        public Keszulek(String gyarto, int ar) {
+                this.gyarto = gyarto;
+                this.ar = ar;
+        }
+
+        public String getGyarto() {
+                return gyarto;
+        }
+
+        public int getAr() {
+                return ar;
+        }
 }
