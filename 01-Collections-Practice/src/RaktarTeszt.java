41a6fab8387ed706fe378fe4fffd1b502ef983fc
uakos03 <urbanakos12@gmail.com>
2026-05-23 18:49:05 +0200

    Lista letrehozva

--- 01-Collections-Practice/src/RaktarTeszt.java
+++ 01-Collections-Practice/src/RaktarTeszt.java
@@ -1,7 +1,10 @@
+import java.util.ArrayList;
+import java.util.List;
 
 public class RaktarTeszt {
     public static void main(String[] args) {
         //Listak gyakorlasa
+        List<OkosTV> tvLista = new ArrayList<OkosTV>();
 
 
     }
