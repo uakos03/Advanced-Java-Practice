7ab6e7b3a76742bcb12634611ae0994451814d80
uakos03 <urbanakos12@gmail.com>
2026-05-23 10:02:43 +0200

    Szarmaztatott osztaly kesz!

--- 01-Collections-Practice/src/OkosTV.java
+++ 01-Collections-Practice/src/OkosTV.java
@@ -1,3 +1,17 @@
 public class OkosTV extends Keszulek {
+        private int kepatlo;
 
+        public OkosTV(String gyarto, int ar, int kepatlo) {
+            super(gyarto,ar);
+            this.kepatlo = kepatlo;
+        }
+
+    public int getKepatlo() {
+        return kepatlo;
+    }
+
+    @Override
+        public String toString() {
+            return super.toString() + " Képátló: " + getKepatlo() + " cm";
+        }
 }
---
+++ 01-Collections-Practice/src/RaktarTeszt.java
@@ -0,0 +1,8 @@
+
+public class RaktarTeszt {
+    public static void main(String[] args) {
+        //Listak gyakorlasa
+
+
+    }
+}
