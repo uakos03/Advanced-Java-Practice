66f5fbff2e0c53dc46268c19872d0a77f1c39c5b
uakos03 <urbanakos12@gmail.com>
2026-05-25 19:57:52 +0200

    Map hozzaadva

--- 01-Collections-Practice/src/RaktarTeszt.java
+++ 01-Collections-Practice/src/RaktarTeszt.java
@@ -1,6 +1,4 @@
-import java.util.ArrayList;
-import java.util.List;
-import java.util.Scanner;
+import java.util.*;
 
 public class RaktarTeszt {
     public static void main(String[] args) {
@@ -12,6 +10,8 @@ public class RaktarTeszt {
         System.out.println(tvDB);
         OkosTV legdragabb = legdragabbTV(tvLista);
         System.out.println(legdragabb);
+
+        Map<String, OkosTV> raktar = new HashMap<>();
 
 
 
