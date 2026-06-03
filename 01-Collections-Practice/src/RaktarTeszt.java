d919c76621e743de97ce2f139ad92f505bfd1a5c
uakos03 <urbanakos12@gmail.com>
2026-06-03 15:26:02 +0200

    HashMap kesz!

--- 01-Collections-Practice/src/RaktarTeszt.java
+++ 01-Collections-Practice/src/RaktarTeszt.java
@@ -4,25 +4,29 @@ public class RaktarTeszt {
     public static void main(String[] args) {
         //Listak gyakorlasa
         List<OkosTV> tvLista = new ArrayList<>();
-        ellenorzottBeolvasas(tvLista);
-        kiIratas(tvLista);
+        Map<String, OkosTV> raktar = ellenorzottBeolvasas(tvLista);
+        kiIratas(tvLista, raktar);
         int tvDB = tvLista.size();
         System.out.println(tvDB);
         OkosTV legdragabb = legdragabbTV(tvLista);
         System.out.println(legdragabb);
+        keresesVonalkodAlapjan(raktar);
 
-        Map<String, OkosTV> raktar = new HashMap<>();
-        //HashMap hozzadasa majd ide!!
-        
+
     }
-    public static void ellenorzottBeolvasas(List<OkosTV> lista) {
+    public static Map<String, OkosTV> ellenorzottBeolvasas(List<OkosTV> lista) {
         Scanner scanner = new Scanner(System.in);
+        Map<String, OkosTV> raktar = new HashMap<>();
         System.out.println("Beolvasó rendszer!");
 
-
-
-
         while (true) {
+            System.out.println("Add meg a TV vonalkódját: (vagy kilépéshez: X)");
+            String vonalkod = scanner.nextLine();
+
+            if(vonalkod.equalsIgnoreCase("X")) {
+                break;
+            }
+
             System.out.println("Add meg a gyártó nevét: (vagy kilépéshez: X)");
             String gyarto = scanner.nextLine();
 
@@ -49,26 +53,50 @@ public class RaktarTeszt {
                     }
                 } while (atlo <= 0);
 
-                lista.add(new OkosTV(gyarto,ar,atlo));
+                OkosTV ujTV = new OkosTV(gyarto, ar, atlo);
+                lista.add(ujTV);
+                raktar.put(vonalkod, ujTV);
 
             } catch (Exception e) {
                 System.out.println("Hiba: " + e.getMessage());
             }
         }
+        return raktar;
     }
 
-    public static void kiIratas(List<OkosTV> lista ) {
-        for (OkosTV elem : lista) {
-            System.out.println(elem);
+    public static void kiIratas(List<OkosTV> lista, Map<String, OkosTV> raktar) {
+        System.out.println("TV lista:");
+        for (OkosTV tv : lista) {
+            System.out.println(tv);
         }
+
+        System.out.println("Raktár tartalma:");
+        System.out.println(raktar);
     }
     public static OkosTV legdragabbTV (List<OkosTV> lista) {
         OkosTV max = lista.get(0);
-        for (int i = 0; i < lista.size(); i++) {
-            if (max.getAr() < lista.get(i).getAr()) {
-                max = lista.get(i);
+        for (OkosTV okosTV : lista) {
+            if (max.getAr() < okosTV.getAr()) {
+                max = okosTV;
             }
         }
             return max;
+    }
+    public static void keresesVonalkodAlapjan(Map<String, OkosTV> raktar) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Azonnali Kereső Rendszer");
+        System.out.println("Add meg a keresett TV vonalkódját: ");
+        String keresettVonalkod = scanner.nextLine();
+
+        if (raktar.containsKey(keresettVonalkod)) {
+
+            OkosTV talaltTV = raktar.get(keresettVonalkod);
+
+            System.out.println("Sikeres találat! A termék adatai:");
+            System.out.println(talaltTV);
+
+        } else {
+            System.out.println("Hiba! Nincs ilyen vonalkódú TV a raktárban.");
+        }
     }
 }
